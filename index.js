97c17d5f914cc5b124e9a21b4e6e616508c90566
Adolf <adolfnyerere@gmail.com>
2023-09-20 20:34:14 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+multiply = 62;
+function multiply () {
+    return (num1 * num2);
+}
+
+multiply();
+let random = Math.ceil(Math.random() * 10) + 1;
+console.log(random);
+let num3 = 20;
+let num4 = 8;
+mod = 4;
+function mod () {
+    return (num3 % num4);
+}
+mod();
+function max (){
+    return Math.max(10,12, 20, 19);
+}
+max();
